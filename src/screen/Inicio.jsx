4b786e0ed0e02884ffe322062239908003da3a72
EdwinVh02 <125947726+EdwinVh02@users.noreply.github.com>
2023-11-21 12:21:27 -0600

    Add files via upload

--- src/screen/Inicio.jsx
+++ src/screen/Inicio.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Inicio = () => {
-  return <div>Inicio</div>;
+  return (
+    <div className="inicio-container">
+      <div className="overlay">
+        <div className="text-container">
+          <h1>Bienvenido a Nuestra Página</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Inicio;
