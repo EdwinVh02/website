572ddce6791b21bb021bc31f23a490c77180cc8d
EdwinVh02 <125947726+EdwinVh02@users.noreply.github.com>
2023-11-26 20:41:08 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import AcercaDe from "./screen/Acercade";
 import PoliticasCookie from "./screen/PolíticadeCookies";
 import PoliticasP from "./screen/PoliticasDePrivacidad";
 import TerminosC from "./screen/TerminosCondiciones";
+import Habitaciones from "./screen/Habitaciones";
+import Login from "./screen/Login";
+import Registrarse from "./screen/Registrarse";
 import Navbar from "./screen/Navbar";
 
 const App = () => {
@@ -13,10 +16,13 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/habitaciones" element={<Habitaciones />} />
         <Route path="/acerca-de" element={<AcercaDe />} />
         <Route path="/politicas-cookies" element={<PoliticasCookie />} />
         <Route path="/politicas-privacidad" element={<PoliticasP />} />
         <Route path="/Terminos-condiciones" element={<TerminosC />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/registrarse" element={<Registrarse />} />
       </Routes>
     </Router>
   );
